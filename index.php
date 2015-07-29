c96e7ff4becf54488abd3fcfe6f152d6c9123c80
chriss <chriss@herocraft.com>
2015-07-29 17:37:02 +0200

    minor comment added

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+// можно вынеести в отдельный файл вроде bootstrap.php, при наличии дополнительных манипуляций перед загрузкой. Оставляю здесь, чтобы не плодить лишние файлы
 spl_autoload_register(function ($className)
 {
     $file = str_replace('\\',DIRECTORY_SEPARATOR, $className);
